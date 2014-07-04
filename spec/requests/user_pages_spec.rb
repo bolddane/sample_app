65b6db5ccf8e160ab451fea6a76c14d1fc3c8a78
bolddane <patrick_obrien53@yahoo.com>
2014-07-04 09:28:26 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,7 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
     end
 
     describe "with valid information" do
@@ -39,9 +40,19 @@ describe "UserPages" do
         fill_in "Confirmation", with: "foobar"
       end
 
-      it "should create a user" do
+     it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-    end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+
+     end
   end
 end
